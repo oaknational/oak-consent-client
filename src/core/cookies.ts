75ea18749605526cec179b683f4861ec417389d0
Kyle Huggins <91190841+k-huggs@users.noreply.github.com>
2026-01-08 15:05:28 +0000

    feat: adds yalc and makes changes value of sameSite for cookies

--- src/core/cookies.ts
+++ src/core/cookies.ts
@@ -5,9 +5,7 @@ const COOKIE_NAME = "oak_consent";
 export function setCookie(value: string) {
   const date = new Date();
   date.setFullYear(date.getFullYear() + 1);
-  document.cookie = `${COOKIE_NAME}=${value}; expires=${date.toUTCString()}; sameSite=lax; path=/; domain=${
-    window.location.hostname
-  }`;
+  document.cookie = `${COOKIE_NAME}=${value}; expires=${date.toUTCString()}; sameSite=none; secure; path=/`;
 }
 export function getCookie() {
   return Cookies.get(COOKIE_NAME);
